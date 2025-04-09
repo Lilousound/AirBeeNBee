9fd2964aff739754dc85e22362a2d74c2f6dec5a
Aurelie Raout <aurelieraout@gmail.com>
2025-04-09 19:48:53 +0200

    new setup done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Bee.destroy_all
+
+puts "Creating bees..."
+bee1 = Bee.create!(
+  name: "Bee 1",
+  speed: 10,
+  amount_honey: 5,
+  address: "123 Bee St.",
+  description: "A fast bee that produces a lot of honey.",
+  price: 100,
+  image_url: "bee1_bho4ho"
+)
+puts "Bee 1 created"
+
+bee2 = Bee.create!(
+  name: "Bee 2",
+  speed: 8,
+  amount_honey: 3,
+  address: "456 Bee Ave.",
+  description: "A slow bee that produces a little honey.",
+  price: 50,
+  image_url: "bee2_z2vpkh"
+)
+puts "Bee 2 created"
+
+puts "Finished!"
