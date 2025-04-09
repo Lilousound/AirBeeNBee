6c8ea4b6d463404946ba894a71a92b11b3653ee8
Aurelie Raout <aurelieraout@gmail.com>
2025-04-09 20:27:51 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,37 @@
+require "open-uri"
+
 puts "Cleaning database..."
 Bee.destroy_all
 
 puts "Creating bees..."
-bee1 = Bee.create!(
-  name: "Bee 1",
-  speed: 10,
-  amount_honey: 5,
-  address: "123 Bee St.",
-  description: "A fast bee that produces a lot of honey.",
-  price: 100,
-  image_url: "bee1_bho4ho"
-)
-puts "Bee 1 created"
 
-bee2 = Bee.create!(
-  name: "Bee 2",
-  speed: 8,
-  amount_honey: 3,
-  address: "456 Bee Ave.",
-  description: "A slow bee that produces a little honey.",
-  price: 50,
-  image_url: "bee2_z2vpkh"
-)
-puts "Bee 2 created"
+
+
+file = URI.parse("https://res.cloudinary.com/dlfmxf1at/image/upload/v1744215782/bee1_bho4ho.jpg").open
+bee = Bee.new(name: "Bee 1", description: "A fast bee that produces a lot of honey.")
+bee.photo.attach(io: file, filename: "bee1_bho4ho.jpg", content_type: "image/jpg")
+bee.save
+
+# bee1 = Bee.create!(
+#   name: "Bee 1",
+#   speed: 10,
+#   amount_honey: 5,
+#   address: "123 Bee St.",
+#   description: "A fast bee that produces a lot of honey.",
+#   price: 100,
+#   image_url: "bee1_bho4ho"
+# )
+# puts "Bee 1 created"
+
+# bee2 = Bee.create!(
+#   name: "Bee 2",
+#   speed: 8,
+#   amount_honey: 3,
+#   address: "456 Bee Ave.",
+#   description: "A slow bee that produces a little honey.",
+#   price: 50,
+#   image_url: "bee2_z2vpkh"
+# )
+# puts "Bee 2 created"
 
 puts "Finished!"
